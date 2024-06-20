1524d5c4632d94dd1cbe17a4b86f9d5609405635
XYZDhiya <140417056+XYZDhiya@users.noreply.github.com>
2024-06-20 20:30:59 +0100

    Clear code

---
+++ app/handlers/commend.map.ts
@@ -0,0 +1,30 @@
+import * as net from "net";
+
+function commendMap(con: net.Socket, com: string[], memory: Map<string, string>) {
+    try {
+        if (com[0].match(/PING/gim)) return con.write("+PONG\r\n");
+        if (com[0].match(/ECHO/gim)) return con.write(`+${com[1]}\r\n`);
+        if (com[0].match(/SET/gim)) {
+            memory.set(com[1], com[2]);
+
+            if (com[3] && com[3].match(/EX|PX/gim)) {
+                const expire: number = parseInt(com[4]) || 0;
+                const time: number = com[3].match(/EX/gim) ? expire * 1000 : expire;
+                setTimeout(() => memory.delete(com[1]), time);
+            }
+
+            return con.write("+OK\r\n");
+        }
+        if (com[0].match(/get/gim)) {
+            const value: string | undefined = memory.get(com[1]);
+            const response: string = value ? `+${value}\r\n` : "$-1\r\n";
+            return con.write(response);
+        }
+
+        return con.write("+ERROR\r\n");
+    } catch (err) {
+        con.write(`-ERR ${err.message}\r\n`);
+    }
+}
+
+export default commendMap;
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import * as net from "net";
 import commendParser from "./handlers/commend.parser";
+import commendMap from "./handlers/commend.map";
 
 // memory database
 const memory: Map<string, string> = new Map();
@@ -7,28 +8,7 @@ const memory: Map<string, string> = new Map();
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data: Buffer) => {
         const commands: string[] = commendParser(data);
-
-        // use regex to parse the command
-        if (commands[0].match(/PING/gim)) return connection.write("+PONG\r\n");
-        if (commands[0].match(/ECHO/gim)) return connection.write(`+${commands[1]}\r\n`);
-        if (commands[0].match(/SET/gim)) {
-            memory.set(commands[1], commands[2]);
-
-            if (commands[3] && commands[3].match(/EX|PX/gim)) {
-                const expire: number = parseInt(commands[4]) || 0;
-                const time: number = commands[3].match(/EX/gim) ? expire * 1000 : expire;
-                setTimeout(() => memory.delete(commands[1]), time);
-            }
-
-            return connection.write("+OK\r\n");
-        }
-        if (commands[0].match(/get/gim)) {
-            const value: string | undefined = memory.get(commands[1]);
-            const response: string = value ? `+${value}\r\n` : "$-1\r\n";
-            return connection.write(response);
-        }
-
-        connection.write("+ERROR\r\n");
+        return commendMap(connection, commands, memory);
     });
 
     connection.on("end", () => {
