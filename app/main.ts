fa32d64868274a47ae19a8219ccf43a1884c83a6
XYZDhiya <140417056+XYZDhiya@users.noreply.github.com>
2024-06-20 19:38:14 +0100

    SET - GET

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,9 @@
 import * as net from "net";
 import commendParser from "./handlers/commend.parser";
 
+// memory database
+const memory: Map<string, string> = new Map();
+
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data: Buffer) => {
         const commands: string[] = commendParser(data);
@@ -8,6 +11,14 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         // use regex to parse the command
         if (commands[0].match(/ping/gim)) return connection.write("+PONG\r\n");
         if (commands[0].match(/echo/gim)) return connection.write(`+${commands[1]}\r\n`);
+        if (commands[0].match(/set/gim)) {
+            memory.set(commands[1], commands[2]);
+            return connection.write("+OK\r\n");
+        }
+        if (commands[0].match(/get/gim)) {
+            const value: string | undefined = memory.get(commands[1]);
+            return connection.write(`+${value}\r\n`);
+        }
 
         connection.write("+ERROR\r\n");
     });
